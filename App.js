51000b2ee70c211335d290be85e9a9c36e12eac5
vara prasad <varaprasad@varas-MacBook-Air.local>
2023-11-29 18:03:27 +0530

    Episode1 - Inception

---
+++ App.js
@@ -0,0 +1,16 @@
+
+    const parent=React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"child"} ,[
+        React.createElement("h1",{},"i'm h1" ),
+        React.createElement("h2",{},"i'm h2" )]),
+
+        React.createElement("div",{id:"child2"} ,[
+            React.createElement("h1",{},"i'm h1" ),
+            React.createElement("h2",{},"i'm h2" )])
+]
+   )
+
+   const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
+    //
