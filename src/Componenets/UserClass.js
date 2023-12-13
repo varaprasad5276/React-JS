4afc8334b2e9bc01a8bec8fc4710be14fb4273be
vara prasad <varaprasad@varas-MacBook-Air.local>
2023-12-13 22:58:22 +0530

    Episode 8 - P2 :React Life Cycle Methods

--- src/Componenets/UserClass.js
+++ src/Componenets/UserClass.js
@@ -4,34 +4,58 @@ class UserClass extends React.Component{
         super(props);
 
         this.state={
-            count:0,
+            userinfo:{
+              name:'dummy',
+              location:'dummy',
+              avatar_url:'http://',
+
+              interval:0
+            }
         };
      console.log(this.props.name +' Child Constructor')
+     
     }
-   componentDidMount(){
-    console.log(this.props.name +'Child component did mount')
+  async componentDidMount(){
+   console.log(this.props.name +'Child component did mount')
+// make an API call
 
+       const data=await fetch("https://api.github.com/users/varaprasad5276");
+       const json=await data.json();
+
+       console.log(json);
+
+       this.setState({
+        userinfo:json})
+
+        this.setState({
+           interval: setInterval(() => {
+                // Update the state of the component
+                this.setState({
+                  count: this.state.count + 1,
+                });
+              }, 3000)
+        })
+
+        
    }
-    
+    componentDidUpdate(){
+            console.log(this.props.name +'Child component did Update')
+
+    }
+    componentWillUnmount(){
+        console.log('UnMount');
+        clearInterval(this.state.interval)
+    }
+
     render(){
-        const {name,location}=this.props;
-        const {count}=this.state;
+        const {name,location,avatar_url}=this.state.userinfo;
         console.log(this.props.name +'Child Render')
+        console.log(name +' Child Render')
 
         return (
             <div className="user-card">
-                <h2>count :{count}</h2>
-            <button 
-            onClick={()=>{
-              //never update state variable directly
-             //{this.state.count=this.state.count+1 like this dont do}
-                this.setState({
-                    count : this.state.count+1,
-                })
-            }}>
-                click
-                </button>   
-            
+             
+            <img src={avatar_url}/>
             <h2>Name : {name }</h2>
             <h3>Contact : GV@mail.com</h3>
             <h4>Location : {location}</h4>
@@ -41,3 +65,22 @@ class UserClass extends React.Component{
 }
 
 export default UserClass;
+            //  -- 1) MOunting Phase
+
+//                   Parent Constructor
+// About.js:18       Parent Render
+// UserClass.js:13       First Child Constructor
+// UserClass.js:35       FirstChild Render
+// UserClass.js:36         dummy Child Render
+// UserClass.js:17       FirstChild component did mount
+// About.js:13       Parent component did mount
+
+         //      -- 2) Updating Phase
+
+// UserClass.js:23         {login: 'varaprasad5276', id: 54410252, node_id: 'MDQ6VXNlcjU0NDEwMjUy', avatar_url: 'https://avatars.githubusercontent.com/u/54410252?v=4', gravatar_id: '', …}
+// UserClass.js:35       FirstChild Render
+// UserClass.js:36       Vara Prasad Child Render
+// UserClass.js:30        FirstChild component did Update
+
+       //       -- 3) UnMounting Phase
+    //  it's called when we go to the new page, it occurs when componenet removed from the DOM
